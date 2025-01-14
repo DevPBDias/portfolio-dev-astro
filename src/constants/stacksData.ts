e96de575b3a9efa72068fcd71bd9370b393416ac
DevPBDias <devpbdias@gmail.com>
2025-01-14 15:48:47 -0300

    feat: create about page

--- src/constants/stacksData.ts
+++ src/constants/stacksData.ts
@@ -1,16 +1,16 @@
-import react from "../assets/images/stacks/react.png";
-import next from "../assets/images/stacks/next.png";
-import node from "../assets/images/stacks/node.png";
-import js from "../assets/images/stacks/js.png";
-import css from "../assets/images/stacks/css.png";
-import html from "../assets/images/stacks/html.png";
-import mongo from "../assets/images/stacks/mongo.png";
-import ts from "../assets/images/stacks/ts.png";
-import tailwind from "../assets/images/stacks/tailwind.png";
-import styled from "../assets/images/stacks/styled.png";
-import sass from "../assets/images/stacks/sass.png";
-import figma from "../assets/images/stacks/figma.png";
-import git from "../assets/images/stacks/git.png";
+import react from "../assets/icons/React.png";
+import next from "../assets/icons/Next.js.png";
+import node from "../assets/icons/Nodejs.png";
+import js from "../assets/icons/JavaScript.png";
+import css from "../assets/icons/CSS3.png";
+import html from "../assets/icons/Html 5.png";
+import mongo from "../assets/icons/Mongo Db.png";
+import ts from "../assets/icons/TypeScript.png";
+import tailwind from "../assets/icons/Tailwind CSS.png";
+import styled from "../assets/icons/Styled.png";
+import sass from "../assets/icons/sass.png";
+import figma from "../assets/icons/figma.png";
+import git from "../assets/icons/git.png";
 
 export const devStacks: ImageMetadata[] = [
   react,
